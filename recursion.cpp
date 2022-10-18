a66457aca5ed9b51781c4c6bb6311427348c0f9e
siddiq <siddiq.aiub2gmail.com>
2022-10-18 17:17:01 +0600

    recirsion function

---
+++ recursion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int recursiveSum(int m, int n){
+    if(m==n)
+        return m;
+    return m + recursiveSum(m+1,n);
+}
+
+int main(){
+    int m=-2,n=5;
+    cout<<recursiveSum(m,n);
+}
