7aa20d864ac426dd3849a41a5c42ff2a32b9b259
siddiq <siddiq.aiub2gmail.com>
2022-10-19 11:23:20 +0600

    triangle, 180 dgree triangle and their reverse

--- patterns/triangle.cpp
+++ patterns/triangle.cpp
@@ -10,18 +10,46 @@ int main(){
     cin>>columns;
     cout<<"Enter symbol: ";
     cin>>symbol;
+    //1. triangle
     cout<<"************triangle************"<<endl;
     for(int i=1;i<=rows;i++){
-        for(int j=i;j<=columns;j++){
-            cout<<symbol;
+        for(int j=i;j<=rows;j++){
+            cout<<symbol<<" ";
         }
         cout<<endl;
     }
-     cout<<"************ reverse triangle************"<<endl;
+    //2. reverse triangle
+    cout<<"************ reverse triangle************"<<endl;
     for(int i=rows;i>0;i--){
-        for(int j=i;j<=columns;j++){
-            cout<<symbol;
+        for(int j=i;j<=rows;j++){ //for(int j=i;j<=row/columns;j++)
+            cout<<symbol<<" ";
         }
+        cout<<endl;
+    }
+    //3. 180 degree triangle
+    cout<<"************triangle 180 degree************"<<endl;
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=rows;j++){
+            if(j<=rows-i){
+                cout<<" ";
+            }else{
+                cout<<symbol<<" ";
+            }
+        }
+
+        cout<<endl;
+    }
+    //3. 180 degree reverse triangle
+    cout<<"************triangle 180 degree************"<<endl;
+    for(int i=rows;i>0;i--){
+        for(int j=1;j<=rows;j++){
+            if(j<=rows-i){
+                cout<<" ";
+            }else{
+                cout<<symbol<<" ";
+            }
+        }
+
         cout<<endl;
     }
 }
