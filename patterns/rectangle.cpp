66d95f1b490bf5a44d86b53c0ac394db946f93a4
siddiq <siddiq.aiub2gmail.com>
2022-10-19 10:07:25 +0600

    reactangle shape

---
+++ patterns/rectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows, columns;
+    cout<<"Number of rows: ";
+    cin>>rows;
+    cout<<"Number of columns: ";
+    cin>>columns;
+
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=columns;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
