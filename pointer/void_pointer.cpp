e23026226d7178abf99440193fc55e4972f5b8dd
siddiq <siddiq.aiub@gmail.com>
2022-10-22 15:07:20 +0600

    void pointer

---
+++ pointer/void_pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void print(void*ptr,char type){
+    switch (type){
+        case 'i': cout<<*((int*)ptr)<<endl;break;
+        case 'c': cout<<*((char*)ptr)<<endl;break;
+    }
+    
+}
+int main(){
+    int number =5;
+    char letter = 's';
+    print(&number,'i');
+    print(&letter,'c');
+    
+}
