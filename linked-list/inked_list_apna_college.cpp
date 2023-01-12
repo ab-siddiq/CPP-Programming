07d3fb5994555ed58c827b32bb00a00d065cf9cd
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2023-01-12 16:45:02 +0600

    insert at head, tail and search in linked list

---
+++ linked-list/inked_list_apna_college.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+//create custom node
+class node{
+    public:
+    //node data
+    int data;
+    //pointer to the next element
+    node* next;
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+//insert at head
+void InsertAtHead(node* &head,int val){
+    node* n = new node(val);
+    n->next=head;
+    head=n;
+}
+//insert at tail
+void InsertAtTail(node* &head,int val){
+    //create new node
+    node* n = new node(val);
+    //if there is no element in the link list or head is empty
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    //creating a pointer to traverse start to end
+    node* temp = head;
+    //traverse till temp->next==NULL
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    //inset at tail after getting last element from while loop
+    temp->next=n;
+}
+//print the link list
+void display(node* head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+//search element
+bool Search(node* head,int key){
+    node* temp = head;
+    while(temp!=NULL){
+        if(temp->data==key) return true;
+        temp=temp->next;
+    }
+    return false;
+}
+int main(){
+    node* head = NULL;
+    InsertAtTail(head,5);
+    InsertAtTail(head,6);
+    InsertAtTail(head,7);
+    InsertAtTail(head,8);
+    display(head);
+    InsertAtHead(head,0);
+    display(head);
+    cout<<Search(head,20);
+}
