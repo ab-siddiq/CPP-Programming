8fc9287024b47d2fc7c4ea702ec9b882a0d38267
ab-siddiq <siddiq.aiub@gmail.com>
2023-01-17 00:36:30 +0600

    insert value at any point

--- linked-list/linked_list_implementation.cpp
+++ linked-list/linked_list_implementation.cpp
@@ -6,6 +6,7 @@ public:
     int data;
     node *next;
 };
+//
 class LinkedList
 {
 public:
@@ -16,6 +17,7 @@ public:
         head = NULL;
         size=0;
     }
+
     // create new node with data=value and next=null
     node *CreateNewNode(int value)
     {
@@ -24,9 +26,11 @@ public:
         newNode->next = NULL;
         return newNode;
     }
+
     // insert new value at head
     void InsertAtHead(int value)
     {
+        //linked list isze
         size++;
         node *a = CreateNewNode(value);
         if (head == NULL)
@@ -38,6 +42,34 @@ public:
         // if head not null
         a->next = head;
         head = a;
+    }
+    //insert at any index
+    void InsertAtAnyIndex(int index, int value){
+        //pointing a pointer to the head
+        node* a = head;
+        int current_index=0;
+        if(index<0 || index>size){
+            return;
+        }
+        //if index is 0 then add in head
+        if(index==0){
+            InsertAtHead(value);
+            return;
+        }
+        //linked list size
+        size++;
+        //move the pointer to index-1 postion
+        while(current_index != index-1){
+            a=a->next;
+            current_index++;
+        }
+        //create a new node
+        node* newNode = CreateNewNode(value);
+        //point the newNode to the index-1 node next
+        newNode->next=a->next;
+        //point index-1 pointer to the newNode
+        a->next=newNode;
+    
     }
     // print the link list
     void Traverse()
@@ -95,5 +127,7 @@ int main()
     l.InsertAtHead(5);
     cout<<l.getSize()<<"\n";
     l.Traverse();
-    l.SearchAllValue(5);
+    l.InsertAtAnyIndex(2,20);
+    cout<<l.getSize()<<"\n";
+    l.Traverse();
 }
