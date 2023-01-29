785557c0e3feb3716ab8b990aa4c400411a2974b
siddiq <siddiq.aiub@gmail.com>
2023-01-29 09:25:15 +0600

    find maximum using recursion

---
+++ lab-mid/5_maximum_using_recursion.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findMax(vector<int> & a, int indx){
+    if(indx==a.size()-1) return a[indx];
+    int maxi=findMax(a,indx+1);
+    if(maxi>a[indx]) return maxi;
+    else return a[indx];
+
+
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr;
+    for(int i=0;i<n;i++){
+        int num;
+        cin>>num;
+        arr.push_back(num);
+    }
+    cout<<findMax(arr,0);
+
+
+}
