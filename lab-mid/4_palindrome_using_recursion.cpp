893e7f0e69a9c4bae59d237734c6b29ab1ef3a29
siddiq <siddiq.aiub@gmail.com>
2023-01-29 09:24:40 +0600

    palindrome using recursion

---
+++ lab-mid/4_palindrome_using_recursion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int checkPalindrome(string str, int s,int e){
+   
+    if(s>=e) return 1;
+    if(str[s]!=str[e]) return 0;
+    return checkPalindrome(str,s+1,e-1);
+}
+int main(){
+    string s;
+    cin>>s;
+    int n = s.size()-1;
+    if(checkPalindrome(s,0,n)) cout<<"Yes\n";
+    else cout<<"No\n";
+   
+}
