0504dbcffcf6f1dfe6ad2ae12a6d5fa5997e1674
abu-bakkar-sebpo <siddiq.aiub@gmail.com>
2023-03-01 12:35:13 +0600

    deque manual sort comparing front back

--- ds-final/6_ladder_array_sort_using_deque.cpp
+++ ds-final/6_ladder_array_sort_using_deque.cpp
@@ -2,13 +2,26 @@
 using namespace std;
 int main(){
     deque<int>dq;
+    deque<int>dqSort;
     int n,v;
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>v;
         dq.push_back(v);
     }
-    sort(dq.begin(),dq.end());
-    for(auto i:dq)
+    int max=0;
+    // sort(dq.begin(),dq.end());
+    // for(auto i:dq)
+    //     cout<<i<<" ";
+    while(!dq.empty()){
+        if(dq.front()>dq.back()){
+            dqSort.push_back(dq.back());
+            dq.pop_back();
+        }else{
+            dqSort.push_back(dq.front());
+            dq.pop_front();
+        }
+    }
+    for(auto i:dqSort)
         cout<<i<<" ";
 }
