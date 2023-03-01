11d76504de43e56638f479b4e3bc6d67ebb372a1
abu-bakkar-sebpo <siddiq.aiub@gmail.com>
2023-03-01 19:14:20 +0600

    delete from heap

--- heap/max_heap_implementation.cpp
+++ heap/max_heap_implementation.cpp
@@ -1,5 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+/*
+    LEFT CHILD OF i 2i+1
+    RIGHT CHILD OF i 2i+2
+    PARENT i-1/2
+*/
 class MaxHeap{
     public:
     vector<int>nodes;
@@ -18,6 +23,25 @@ class MaxHeap{
         nodes.push_back(value);
         up_heapify(nodes.size()-1);
     }
+    void down_heapify(int idx){
+        while (1)
+        {
+            int largest = idx;
+            int l = 2*idx+1;
+            int r = 2*idx+2;
+            if(l<nodes.size() && nodes[largest]<nodes[l]) largest=l;
+            if(r<nodes.size() && nodes[largest]<nodes[r]) largest=r;
+            if(largest==idx) break;
+            swap(nodes[idx],nodes[largest]);
+            idx=largest;
+        }
+        
+    }
+    void Delete(int idx){
+        swap(nodes[idx],nodes[nodes.size()-1]);
+        nodes.pop_back();
+        down_heapify(idx);
+    }
     void PrintHeap(){
         for(int i=0;i<nodes.size();i++){
             cout<<nodes[i]<<" ";
@@ -31,7 +55,11 @@ int main(){
     mh.insert(7);
     mh.insert(4);
     mh.insert(6);
-    mh.insert(9);
-    mh.insert(11);
+    mh.insert(5);
+    mh.insert(3);
+    mh.insert(2);
+    // mh.insert(12);
+    mh.PrintHeap();
+    mh.Delete(0);
     mh.PrintHeap();
 }
