683d58ebef7e63fbb81a522cb7fa9496179de4e3
abu-bakkar-sebpo <siddiq.aiub@gmail.com>
2023-04-03 15:02:30 +0600

    graph representation using adjacency list

---
+++ graph/graph_representation_using_adjacency_list.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+    vector<int>v[5]
+    meaning of above syntex is vector inside vector
+    example-
+    v[1].push_back(2);
+    v[1].push_back(5);
+    v[1].push_back(1);
+    meaning is in the position of v1, elements are 2,5,1
+*/
+int main()
+{
+    int nodes,edges;
+    cin>>nodes>>edges;
+    vector<int>adjacencyList[nodes+1];
+    for(int i=1;i<=edges;i++){
+        int u,v;
+        cin>>u>>v;
+        adjacencyList[u].push_back(v);
+        adjacencyList[v].push_back(u);
+    }
+    for(int i=1;i<=nodes;i++){
+        cout<<i<<" -> ";
+        for(auto list:adjacencyList[i])
+            cout<<list<<" ";
+        cout<<"\n";
+    }
+}
