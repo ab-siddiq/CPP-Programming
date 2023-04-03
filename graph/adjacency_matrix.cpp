45e8d357d8fe54441046b5c930ef3f3053880250
abu-bakkar-sebpo <siddiq.aiub@gmail.com>
2023-04-03 14:14:13 +0600

    graph representation using adjacency matrix

---
+++ graph/adjacency_matrix.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int adjacencyMatrix[n+1][m+1];
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            adjacencyMatrix[i][j]=0;
+        }
+    }
+    for(int i=1;i<=m;i++){
+        int u,v;
+        cin>>u>>v;
+        adjacencyMatrix[u][v]=1;
+        adjacencyMatrix[v][u]=1;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++)
+            cout<<adjacencyMatrix[i][j]<<" ";
+        cout<<"\n";
+    }
+}
