648e3c33abc8cff6a1d12da7c4c234255a20f61c
abu-bakkar-sebpo <siddiq.aiub@gmail.com>
2023-04-05 14:27:47 +0600

    dijkstra algorithm modified with comment

--- algorithms/dijkstra_algorithm.cpp
+++ algorithms/dijkstra_algorithm.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e5+5;
-// const long long int INF = 1e18;
-const int INF = 1e9;
 int nodes,edges;
-vector<pair<int,int>> adj_list[N];
+const int maxNode = 1e5+5;
+const int INF = 1e5;
+vector<pair<int,int>>adjacencyList[maxNode];
+int d[maxNode],visited[maxNode];
 /*
             w
     u ---------- > v
@@ -13,41 +13,50 @@ vector<pair<int,int>> adj_list[N];
     u -> (v,w), (x,a)
 
 */
-int d[N], visited[N];
 void dijkstra(int src){
     for(int i=1;i<=nodes;i++){
+        //initialize all node to infinity
         d[i]=INF;
     }
+    //initialize the starting node to zero
     d[src]=0;
     for(int i=0;i<nodes;i++){
         int selectedNode = -1;
         for(int j=1;j<=nodes;j++){
+            //if visited then skip
             if(visited[j]==1) continue;
-            if(selectedNode==-1 || d[selectedNode]>d[j]){
-                selectedNode = j;
+            //pick a unvisited node with minimum distance
+            // selectedNode==-1 means no node is selected yet
+            if(selectedNode==-1 || d[selectedNode] > d[j]){
+                selectedNode=j;
             }
         }
+        //make the selected node as visited
         visited[selectedNode]=1;
-        for(auto adj_entry:adj_list[selectedNode]){
+        //traverse all adjacent node of selected node
+        for(auto adj_entry:adjacencyList[selectedNode]){
             int adj_node = adj_entry.first;
-            int edge_cst = adj_entry.second;
-            if(d[selectedNode]+edge_cst < d[adj_node]){
-
+            int edge_cost = adj_entry.second;
+            // if the the selected node cost is less then current node 
+            if(d[selectedNode]+edge_cost <d[adj_node]){
+                //then change currrent node cost to selected node cost
+                d[adj_node]=d[selectedNode]+edge_cost;
             }
         }
     }
 }
 int main(){
     cin>>nodes>>edges;
-    for(int i=0;i<edges;i++){
+    for(int i=1;i<=edges;i++){
         int u,v,w;
-        cin>>u>>v>>w ;
-        adj_list[u].push_back({v,w});
-        adj_list[v].push_back({u,w});
+        cin>>u>>v>>w;
+        adjacencyList[u].push_back({v,w});
+        adjacencyList[v].push_back({u,w});
     }
-    int src=1;
-    dijkstra(src);
+    dijkstra(1);
     for(int i=1;i<=nodes;i++){
-       cout<<d[i]<<" "; 
+        cout<<d[i]<<" ";
     }
+    cout<<"\n";
+
 }
