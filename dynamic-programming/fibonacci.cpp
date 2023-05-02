74d460a05e13d58920a40a3c3924b6ca2cf13ed4
abu-bakkar-sebpo <siddiq.aiub@gmail.com>
2023-05-02 16:28:57 +0600

    modify base case for fibonacci memoization method

--- dynamic-programming/fibonacci.cpp
+++ dynamic-programming/fibonacci.cpp
@@ -1,8 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long arr[102];
+const int N=101;
+long long arr[N];
 long long fib(int n){
-    if(n<=2) return 1;
+    if(n==0) return 0;
+    if(n>0 && n<=2) return 1;
     if(arr[n]!=0) return arr[n];
 
     arr[n] = fib(n-1)+fib(n-2);
